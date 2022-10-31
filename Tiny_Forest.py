14edbc89af9c17fb3c0d96e9f608fef649f539f3
GitaePark <rlxo8749@gmail.com>
2022-10-31 18:52:00 +0900

    작은 숲1층 타일에 맞게 배치(출력)

--- Tiny_Forest.py
+++ Tiny_Forest.py
@@ -1,15 +1,24 @@
 from pico2d import *
 import game_framework
 import play_state
+from Map import TinyFores1f
 
 image = None
+timage = None
 aron = None
+imageArray = None
+printImageX = 10
+printImageY = 6
 
 def enter():
     global image
+    global timage
     global aron
+    global imageArray
     aron = play_state.Aron(28*10, 28*10)
     image = load_image('Map\\TinyForest_1f.png')
+    timage = load_image('Map\\TinyForest_Tile.png')
+    imageArray = TinyFores1f.TinyForest_1f
     pass
 
 def exit():
@@ -24,7 +33,16 @@ def update():
 
 def draw():
     clear_canvas()
-    image.draw(1344//2, 700//2)
+
+    for i in range(25):
+        for j in range(48):
+            if imageArray[i][j] == 16:
+                timage.clip_draw(24 * (16%8), 24 * (16//8), 24, 24, i*28*2, j*28*2, 28*2, 28*2)
+            if imageArray[i][j] == 23:
+                timage.clip_draw(24 * (23%8), 24 * (23//8), 24, 24, i*28*2, j*28*2, 28*2, 28*2)
+            pass
+
+    #image.draw(1344//2, 700//2)
     aron.draw()
     update_canvas()
     pass
