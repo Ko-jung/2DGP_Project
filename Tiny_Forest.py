29df4c8fb67dddc89f047f0086480675e15be26c
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-11-03 01:48:58 +0900

    주인공 근처로 맵생성 코드

--- Tiny_Forest.py
+++ Tiny_Forest.py
@@ -1,24 +1,48 @@
 from pico2d import *
+from Pokemon.pikachu import Pikachu
 import game_framework
-import play_state
-from Map import TinyFores1f
+from Define import *
+from Map import MtSteel
+from Map import TinyForest
+
+import random
 
 image = None
 timage = None
-aron = None
+pika = None
 imageArray = None
-printImageX = 10
-printImageY = 6
 
 def enter():
     global image
     global timage
-    global aron
     global imageArray
-    aron = play_state.Aron(28*10, 28*10)
-    image = load_image('Map\\TinyForest_1f.png')
-    timage = load_image('Map\\TinyForest_Tile.png')
-    imageArray = TinyFores1f.TinyForest_1f
+    global pika
+
+    pika = Pikachu()
+    image = load_image('Map\\Image\\TinyForest_1f.png')
+    timage = load_image('Map\\Image\\TinyForest_Tile.png')
+
+    imageArray = TinyForest.TinyForest1f
+
+    for i in range(48):
+        for j in range(25):
+            if imageArray[24 - j][i] == 23:
+                match random.randint(0, 2):
+                    case 0:
+                        imageArray[24 - j][i] = 23
+                        pass
+                    case 1:
+                        imageArray[24 - j][i] = 4
+                        pass
+                    case 2:
+                        imageArray[24 - j][i] = 5
+                        pass
+            elif imageArray[24 - j][i] == 14:
+                if random.randint(0, 10) <= 1:
+                        imageArray[24 - j][i] = 6
+            elif imageArray[24 - j][i] == 16:
+                if random.randint(0, 10) <= 0:
+                    imageArray[24 - j][i] = 17
     pass
 
 def exit():
@@ -27,23 +51,31 @@ def exit():
     pass
 
 def update():
-    aron.update()
-    delay(0.3)
+    pika.update()
+    delay(0.1)
     pass
 
 def draw():
     clear_canvas()
 
-    for i in range(25):
-        for j in range(48):
-            if imageArray[i][j] == 16:
-                timage.clip_draw(24 * (16%8), 24 * (16//8), 24, 24, i*28*2, j*28*2, 28*2, 28*2)
-            if imageArray[i][j] == 23:
-                timage.clip_draw(24 * (23%8), 24 * (23//8), 24, 24, i*28*2, j*28*2, 28*2, 28*2)
-            pass
+    for i in range(pika.x - printImageX, pika.x + printImageX + 1):
+        for j in range(pika.y - printImageY, pika.y + printImageY + 1):
+            if i < 48 and i >= 0 and j < 25 and j >= 0:
+                timage.clip_draw(24 * (imageArray[24 - j][i] % 8), 24 * (imageArray[24 - j][i] // 8), 24, 24,
+                                 (i - pika.x + printImageX) * 28 * printSize + 14 * printSize,
+                                 (j - pika.y + printImageY + 1) * 28 * printSize - 14 * printSize,
+                                 28 * printSize, 28 * printSize)
+            else:
+                timage.clip_draw(24 * (23 % 8), 24 * (23 // 8), 24, 24,
+                                 (i - pika.x + printImageX) * 28 * printSize + 14 * printSize,
+                                 (j - pika.y + printImageY + 1) * 28 * printSize - 14 * printSize,
+                                 28 * printSize, 28 * printSize)
 
-    #image.draw(1344//2, 700//2)
-    aron.draw()
+    # for i in range(48):
+    #     for j in range(25):
+    #         timage.clip_draw(24 * (imageArray[24 - j][i] % 8), 24 * (imageArray[24 - j][i] // 8), 24, 24, i * 28, j * 28, 28, 28)
+
+    pika.draw()
     update_canvas()
     pass
 
@@ -52,10 +84,11 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                game_framework.quit()
-                #game_framework.change_state(logo_state)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        else:
+            print('TIny_forest handleevents')
+            pika.handle_event(event) # 소년한테 이벤트를 처리하도록 넘겨준다
 
 
 
