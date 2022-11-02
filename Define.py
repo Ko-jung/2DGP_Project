5034cd049f54e9946213c27c4b4d12f9b3762027
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-11-03 01:49:36 +0900

    c의 전처리기 같은 기능하는 define 파일 추가

---
+++ Define.py
@@ -0,0 +1,9 @@
+from Map.MtSteel import *
+from Map.TinyForest import *
+
+printImageX = 7
+printImageY = 4
+printSize = 3
+
+TinyForestArr = [TinyForest1f, TinyForest2f, TinyForest3f]
+MtSteelArr = [MtSteel1f, MtSteel2f, MtSteel3f, MtSteel4f, MtSteel5f, MtSteel6f, MtSteel7f, MtSteel8f, MtSteel9f]
