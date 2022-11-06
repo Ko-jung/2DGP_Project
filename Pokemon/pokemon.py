48b92ff1380509e812d596f9d1c441f63c016b50
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-11-06 23:31:53 +0900

    포켓몬의 좌표 이동 관련 함수 추가

--- Pokemon/pokemon.py
+++ Pokemon/pokemon.py
@@ -1,6 +1,5 @@
-
-DIR_NE, DIR_E, DIR_SE, DIR_N, DIR_NW, DIR_W, DIR_SW, DIR_S = range(8)
-
+from pico2d import *
+from game_world import *
 
 class Pokemon:
     def __init__(self):
@@ -34,8 +33,56 @@ class Pokemon:
         self.image.clip_draw(1 + 29 * self.frame, 1 + 29 * self.dir, 28, 28, (28 * 3 * 15) // 2, (28 * 3 * 9) // 2, 28 * 3, 28 * 3)
         pass
 
-    def handle_event(self):
+    def handle_event(self, event):
+        if event.type == SDL_KEYDOWN:
+            if event.key == pico2d.SDLK_UP:
+                self.dirY += 1
+            if event.key == pico2d.SDLK_DOWN:
+                self.dirY -= 1
+            if event.key == pico2d.SDLK_LEFT:
+                self.dirX -= 1
+            if event.key == pico2d.SDLK_RIGHT:
+                self.dirX += 1
+        elif event.type == SDL_KEYUP:
+            if event.key == pico2d.SDLK_UP:
+                self.dirY -= 1
+            if event.key == pico2d.SDLK_DOWN:
+                self.dirY += 1
+            if event.key == pico2d.SDLK_LEFT:
+                self.dirX += 1
+            if event.key == pico2d.SDLK_RIGHT:
+                self.dirX -= 1
         pass
+
+    def moveToPos(self, XY):
+        self.x, self.y = XY[0], XY[1]
+        pass
+
+    def overMap(self, x = 0, y = 0):
+        if self.dir == DIR_NE:
+            self.x += -1
+            self.y += -1
+        elif self.dir == DIR_E:
+            self.x += -1
+            self.y +=  0
+        elif self.dir == DIR_SE:
+            self.x += -1
+            self.y +=  1
+        elif self.dir == DIR_N:
+            self.x +=  0
+            self.y += -1
+        elif self.dir == DIR_S:
+            self.x +=  0
+            self.y +=  1
+        elif self.dir == DIR_NW:
+            self.x +=  1
+            self.y += -1
+        elif self.dir == DIR_W:
+            self.x +=  1
+            self.y +=  0
+        elif self.dir == DIR_SW:
+            self.x +=  1
+            self.y +=  1
 
 
 
