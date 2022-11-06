3112640e30932a4531dcc0c0284f969fe92da53b
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-11-06 23:34:15 +0900

    map 클래스 생성으로 인한 리팩토링

--- Tiny_Forest.py
+++ Tiny_Forest.py
@@ -1,81 +1,86 @@
 from pico2d import *
 from Pokemon.pikachu import Pikachu
 import game_framework
-from Define import *
+from game_world import *
+from Map import map
 from Map import MtSteel
 from Map import TinyForest
 
 import random
 
-image = None
 timage = None
 pika = None
 imageArray = None
+backGround = None
+floor = None
 
 def enter():
-    global image
     global timage
     global imageArray
     global pika
+    global floor
+    global backGround
 
-    pika = Pikachu()
-    image = load_image('Map\\Image\\TinyForest_1f.png')
+    imageArray = TinyForest.TinyArray
     timage = load_image('Map\\Image\\TinyForest_Tile.png')
+    floor = 0
 
-    imageArray = TinyForest.TinyForest1f
+    randomPos = [[] for c in range(len(imageArray))]
 
-    for i in range(48):
-        for j in range(25):
-            if imageArray[24 - j][i] == 23:
-                match random.randint(0, 2):
-                    case 0:
-                        imageArray[24 - j][i] = 23
-                        pass
-                    case 1:
-                        imageArray[24 - j][i] = 4
-                        pass
-                    case 2:
-                        imageArray[24 - j][i] = 5
-                        pass
-            elif imageArray[24 - j][i] == 14:
-                if random.randint(0, 10) <= 1:
-                        imageArray[24 - j][i] = 6
-            elif imageArray[24 - j][i] == 16:
-                if random.randint(0, 10) <= 0:
-                    imageArray[24 - j][i] = 17
+    for n in range(len(imageArray)):
+        for i in range(48):
+            for j in range(25):
+                if imageArray[n][24 - j][i] == 23:
+                    match random.randint(0, 2):
+                        case 0:
+                            imageArray[n][24 - j][i] = 23
+                            pass
+                        case 1:
+                            imageArray[n][24 - j][i] = 4
+                            pass
+                        case 2:
+                            imageArray[n][24 - j][i] = 5
+                            pass
+                elif imageArray[n][24 - j][i] == 14:
+                    if random.randint(0, 10) <= 1:
+                        imageArray[n][24 - j][i] = 6
+                elif imageArray[n][24 - j][i] == 16:
+                    randomPos[n].append([i, j])
+                    if random.randint(0, 10) <= 0:
+                        imageArray[n][24 - j][i] = 17
+
+    backGround = map.Map(imageArray, timage, floor, randomPos)
+
+    pika = Pikachu(randomPos[floor][random.randint(0, len(randomPos[floor]))])
+    add_object(pika, MAINOBJECT)
     pass
 
 def exit():
-    global image
-    del image
+    clearAI()
     pass
 
 def update():
-    pika.update()
+    for o in all_objects():
+           o.update()
+    backGround.update()
+    if backGround.isOverMap(objects[MAINOBJECT][0]):
+        objects[MAINOBJECT][0].overMap()
+        pass
+
+
     delay(0.1)
     pass
 
+def draw_world():
+    backGround.draw(objects[MAINOBJECT][0].x, objects[MAINOBJECT][0].y)
+
+    # for layer in game_world.objects:
+    for o in all_objects():
+           o.draw()
+
 def draw():
     clear_canvas()
-
-    for i in range(pika.x - printImageX, pika.x + printImageX + 1):
-        for j in range(pika.y - printImageY, pika.y + printImageY + 1):
-            if i < 48 and i >= 0 and j < 25 and j >= 0:
-                timage.clip_draw(24 * (imageArray[24 - j][i] % 8), 24 * (imageArray[24 - j][i] // 8), 24, 24,
-                                 (i - pika.x + printImageX) * 28 * printSize + 14 * printSize,
-                                 (j - pika.y + printImageY + 1) * 28 * printSize - 14 * printSize,
-                                 28 * printSize, 28 * printSize)
-            else:
-                timage.clip_draw(24 * (23 % 8), 24 * (23 // 8), 24, 24,
-                                 (i - pika.x + printImageX) * 28 * printSize + 14 * printSize,
-                                 (j - pika.y + printImageY + 1) * 28 * printSize - 14 * printSize,
-                                 28 * printSize, 28 * printSize)
-
-    # for i in range(48):
-    #     for j in range(25):
-    #         timage.clip_draw(24 * (imageArray[24 - j][i] % 8), 24 * (imageArray[24 - j][i] // 8), 24, 24, i * 28, j * 28, 28, 28)
-
-    pika.draw()
+    draw_world()
     update_canvas()
     pass
 
