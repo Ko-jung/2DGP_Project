72512940db8b3551a26f3bfa6b195c51e6a78543
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-11-10 21:22:50 +0900

    IDLE 상태 추가

--- Pokemon/pokemon.py
+++ Pokemon/pokemon.py
@@ -17,6 +17,30 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 2
 
+class IDLE:
+    @staticmethod
+    def enter(self, event):
+        print('ENTER IDLE')
+        self.dir = 0
+        self.timer = 1000
+
+    @staticmethod
+    def exit(self, event):
+        print('EXIT IDLE')
+
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % self.fullFrame
+        self.timer -= 1
+
+
+    @staticmethod
+    def draw(self):
+        if self.face_dir == 1:
+            self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(int(self.frame) * 100, 200, 100, 100, self.x, self.y)
+
 class Pokemon:
     def __init__(self):
         self.x, self.y = 0, 0
